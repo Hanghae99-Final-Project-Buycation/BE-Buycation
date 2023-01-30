435026818ebeeff63d05a6b10f4a6ed0364de170
syckor <syckor@naver.com>
2023-01-30 11:57:47 +0900

    Fix : Security config 복구

---
+++ Buycation/src/main/java/com/example/buycation/security/config/WebSecurityConfig.java
@@ -0,0 +1,101 @@
+package com.example.buycation.security.config;
+
+import com.example.buycation.common.ResponseMessage;
+import com.example.buycation.security.jwt.JwtAuthFilter;
+import com.example.buycation.security.jwt.JwtUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import static com.example.buycation.common.exception.ErrorCode.FORBIDDEN_ERROR;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    private final JwtUtil jwtUtil;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+//                .requestMatchers(PathRequest.toH2Console())
+                .antMatchers("/ws/**", "/send/**", "/talk/**")
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.POST, new String[]{"/api/members/signup", "/api/members/login"}).permitAll()
+                .antMatchers(HttpMethod.GET, new String[]{"/api/posting/**", "/api/members/signup/**","/api/members/{memberId}/profile","/api/members/login/kakao"}).permitAll()
+                .antMatchers(HttpMethod.PUT, "/api/members/signup/emailcheck").permitAll()
+                .anyRequest().authenticated()
+                .and().addFilterBefore(new JwtAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+        http.exceptionHandling()
+                .authenticationEntryPoint(unauthorizedEntryPoint)
+                .and().headers().frameOptions().disable()
+                .and().cors();
+
+        return http.build();
+    }
+
+    private final AuthenticationEntryPoint unauthorizedEntryPoint =
+            (request, response, authException) -> {
+                response.setStatus(HttpStatus.FORBIDDEN.value());
+                String json = new ObjectMapper().writeValueAsString(new ResponseMessage<>(FORBIDDEN_ERROR, FORBIDDEN_ERROR));
+                response.setContentType("application/json; charset=utf8");
+                response.getWriter().write(json);
+            };
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.addAllowedOrigin("http://localhost:3000");
+
+        config.addAllowedOrigin("https://www.buycation.co.kr");
+
+        config.addAllowedOrigin("https://buycation.co.kr");
+
+        config.addExposedHeader(JwtUtil.AUTHORIZATION_HEADER);
+
+        config.addAllowedMethod("*");
+
+        config.addAllowedHeader("*");
+
+        config.setAllowCredentials(true);
+
+        config.validateAllowCredentials();
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+}
