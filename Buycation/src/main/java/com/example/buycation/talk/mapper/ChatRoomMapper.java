1f58a80e22a6bf944cab5d262816b49df90b310e
syckor <syckor@naver.com>
2023-01-30 13:16:34 +0900

    Fix : 채팅 내용 last index 위치 변경

--- Buycation/src/main/java/com/example/buycation/talk/mapper/ChatRoomMapper.java
+++ Buycation/src/main/java/com/example/buycation/talk/mapper/ChatRoomMapper.java
@@ -9,7 +9,7 @@ import java.time.format.DateTimeFormatter;
 @Component
 public class ChatRoomMapper {
     public ChatRoomResponseDto toTalkRoomResponseDto(ChatRoom talkRoom){
-        int talkSize = talkRoom.getTalks().size()>0?talkRoom.getTalks().size():0;
+        int lastIndex = talkRoom.getTalks().size()>0?talkRoom.getTalks().size()-1:0;
         return ChatRoomResponseDto.builder()
                 .id(talkRoom.getId())
                 .postingId(talkRoom.getPosting().getId())
@@ -17,8 +17,8 @@ public class ChatRoomMapper {
                 .image(talkRoom.getPosting().getImage())
                 .currentMembers(talkRoom.getPosting().getCurrentMembers())
                 .totalMembers(talkRoom.getPosting().getTotalMembers())
-                .lastMessage(talkSize>0 ? talkRoom.getTalks().get(talkSize).getMessage() : "대화 내용이 없습니다.")
-                .lastReceiveTime(talkSize>0 ? talkRoom.getTalks().get(talkSize).getCreatedAt()
+                .lastMessage(lastIndex > 0 ? talkRoom.getTalks().get(lastIndex).getMessage() : "대화 내용이 없습니다.")
+                .lastReceiveTime(lastIndex > 0 ? talkRoom.getTalks().get(lastIndex).getCreatedAt()
                         .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")) : "")
                 .dueDate(talkRoom.getPosting().getDueDate())
                 .build();
