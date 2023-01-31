132daa3fa311bdeff866cdd7abb3df5e08e3e94a
jihunBeak <115356113+jihun1362@users.noreply.github.com>
2023-01-31 14:39:23 +0900

    Update WebSocketConfig.java

--- Buycation/src/main/java/com/example/buycation/talk/WebSocketConfig.java
+++ Buycation/src/main/java/com/example/buycation/talk/WebSocketConfig.java
@@ -19,13 +19,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS().setHeartbeatTime(1000);;
-    }
-
-    @Override
-    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
-        registration.setMessageSizeLimit(160 * 64 * 1024); // default : 64 * 1024
-        registration.setSendTimeLimit(100 * 10000); // default : 10 * 10000
-        registration.setSendBufferSizeLimit(3* 512 * 1024); // default : 512 * 1024
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
     }
 }
