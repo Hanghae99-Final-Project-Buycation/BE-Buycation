2c457287621a56d565e84460e178ddf67771880b
jihunBeak <115356113+jihun1362@users.noreply.github.com>
2023-01-31 14:34:19 +0900

    Update WebSocketConfig.java

--- Buycation/src/main/java/com/example/buycation/talk/WebSocketConfig.java
+++ Buycation/src/main/java/com/example/buycation/talk/WebSocketConfig.java
@@ -1,12 +1,11 @@
 package com.example.buycation.talk;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -20,7 +19,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS().setHeartbeatTime(1200);
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
     }
 
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+        registration.setMessageSizeLimit(160 * 64 * 1024); // default : 64 * 1024
+        registration.setSendTimeLimit(100 * 10000); // default : 10 * 10000
+        registration.setSendBufferSizeLimit(3* 512 * 1024); // default : 512 * 1024
+    }
 }
