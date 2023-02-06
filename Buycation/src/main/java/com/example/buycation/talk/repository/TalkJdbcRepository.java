ca82af9081164408175a31714410593ffba2635c
syckor <syckor@naver.com>
2023-02-06 14:40:23 +0900

    Fix : api 타임 아웃 설정

--- Buycation/src/main/java/com/example/buycation/talk/repository/TalkJdbcRepository.java
+++ Buycation/src/main/java/com/example/buycation/talk/repository/TalkJdbcRepository.java
@@ -28,11 +28,6 @@ public class TalkJdbcRepository {
             public void setValues(PreparedStatement ps, int i) throws SQLException {
                 TalkRedisDto talkRedisDto = talkRedisDtos.get(i);
 
-                System.out.println("1 : " + talkRedisDto.getSendDate());
-                System.out.println("2 : " + talkRedisDto.getMessage());
-                System.out.println("3 : " + talkRedisDto.getTalkRoomId());
-                System.out.println("4 : " + talkRedisDto.getMemberId());
-
                 ps.setTimestamp(1, Timestamp.valueOf(talkRedisDto.getSendDate()));
                 ps.setString(2, talkRedisDto.getMessage());
                 ps.setLong(3, talkRedisDto.getTalkRoomId());
